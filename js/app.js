195c6cc188c1fa939d80e49f7b7e7b859d1a920d
Walter Sylvester <walter_sylvester@msn.com>
2019-10-07 17:30:44 -0600

    added theplayer image to the screen

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+$('.images').html('<img id="gamePic" src="https://i.imgur.com/COUC6oe.png" />');
+// This should produce the same thing as above except there is no id
+// $('.images').attr('src','https://i.imgur.com/gEnud9A.png');
+
+// setting the game picture dimensions
+$('#gamePic').width(200);
+$('#gamePic').height(300);
+
