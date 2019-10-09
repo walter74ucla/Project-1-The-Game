f459301a8901a2776903f50c06815afc4fee46fb
Walter Sylvester <walter_sylvester@msn.com>
2019-10-08 18:29:41 -0600

    created the random feature for game image selection

--- js/app.js
+++ js/app.js
@@ -16,6 +16,58 @@
 $('#gamePic').width(200);
 $('#gamePic').height(300);
 
+let avgLeaders = [
+	{
+		firstName: 'Ty',
+		lastName: 'Cobb',
+		avg: 0.366,
+		clue1: '12 Batting Titles',
+		clue2: '1 Triple Crown and 1 MVP win',
+		clue3: 'Nicknamed the Georgia Peach',
+		image: src="https://i.imgur.com/COUC6oe.png"
+	 }, {
+	 	firstName: 'Rogers',
+	 	lastName: 'Hornsby',
+	 	avg: 0.358,
+	 	clue1: '7 Batting Titles',
+	 	clue2: '2 Triple Crown and 7 MVP wins',
+	 	clue3: 'Nicknamed Rajah',
+	 	image: src="https://i.imgur.com/99dWUcv.jpg"
+	 }, {
+	 	firstName: 'Shoeless Joe',
+	 	lastName: 'Jackson',
+	 	avg: 0.356,
+	 	clue1: '13 year career',
+	 	clue2: 'Played for PHA, CLE, and CWS',
+	 	clue3: '3 time AL Triples leader',
+	 	image: src="https://i.imgur.com/pDcYqA4.jpg"
+	 }
+]
+
+let numAvgPics = 5;
+const game = {
+	displayAvgPics(){
+		let orderArray = [];
+		for(let i=0; i<numAvgPics; i++){
+			let orderNum = i;
+			orderArray.push(orderNum);	
+		}
+			console.log(orderArray);
+			let spliceArray = []; //use this to hold the splice			
+			let randomIndexArray = [];
+			let j = 0;
+			while(j < orderArray.length){
+				let index = Math.floor(Math.random() * orderArray.length);				
+				spliceArray = orderArray.splice(index, 1);
+				// console.log(spliceArray);
+				randomIndexArray.push(spliceArray[0]);	
+			}
+				console.log(randomIndexArray);
+	}
+}
+game.displayAvgPics();
+
+
 const newImage = () => {
 	$('#gameImage').attr('src','https://i.imgur.com/COUC6oe.png');	
 }
@@ -38,6 +90,33 @@ const $showCover = $('#showCover');
 $showCover.on('click', () => {
 	// console.log('button worked');
 	newCover();
+});
+
+const removeClue1 = () => {
+	$('#clue1').css('opacity', 0);
+}
+
+const $clue1 = $('#clue1');
+$clue1.on('click', () => {
+	removeClue1();
+});
+
+const removeClue2 = () => {
+	$('#clue2').css('opacity', 0);
+}
+
+const $clue2 = $('#clue2');
+$clue2.on('click', () => {
+	removeClue2();
+});
+
+const removeClue3 = () => {
+	$('#clue3').css('opacity', 0);
+}
+
+const $clue3 = $('#clue3');
+$clue3.on('click', () => {
+	removeClue3();
 });
 
 
